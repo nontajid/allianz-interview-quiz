dd0db9c590d31309a18e7f5c7885ab879d65ab3a
Nontajid Pantarak <nontajid.pantarak@allianz.com>
2019-04-29 11:32:22 +0700

    correct age validation

--- src/app/shared/form-validator.ts
+++ src/app/shared/form-validator.ts
@@ -1,14 +1,17 @@
 import { ValidatorFn, AbstractControl } from '@angular/forms';
 
-export function ageValidator(age: number): ValidatorFn {
+export function ageValidator(minAge: number): ValidatorFn {
     return (control: AbstractControl): {[key: string]: any} | null => {
         let result: {[key: string]: any} | null = null;
 
         if (control.value) {
             const ageDiffMs = Date.now() - new Date(control.value).getTime();
             const ageDate = new Date(ageDiffMs);
-            const isOverAgeLimit = (Math.abs(ageDate.getUTCFullYear() - 1970)) >= age;
-            result = !isOverAgeLimit ? {ageUnderLimit: {ageLimit: age}} : null;
+            const currentAge = ageDate.getUTCFullYear() - 1970;
+            console.log(currentAge);
+            
+            const isOverAgeLimit = currentAge >= minAge;
+            result = !isOverAgeLimit ? {ageUnderLimit: {ageLimit: minAge}} : null;
         }
 
         return result;
